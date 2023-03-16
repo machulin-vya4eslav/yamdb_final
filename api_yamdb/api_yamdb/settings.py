ef8e2fb4f21f8b3d22ae89e922ec14b2bad6bbf6
Вячеслав Мачулин <machulin.vya4eslav@ya.ru>
2023-03-17 00:18:12 +0300

    tests_build_deploy_message

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'abrakadabra')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'True')
+DEBUG = os.getenv('DEBUG', 'False')
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost')
 
