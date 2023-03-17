8573916533b07ea87692e1fac8d545efade8cd48
Вячеслав Мачулин <machulin.vya4eslav@ya.ru>
2023-03-17 14:22:04 +0300

    tests and build_and_push_to_docker_hub

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -4,7 +4,8 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', 'abrakadabra')
+# SECRET_KEY = os.getenv('SECRET_KEY', 'abrakadabra')
+SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', False)
