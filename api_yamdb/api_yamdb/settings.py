723e99802ff2a551724bedbe9391ed1367dd77de
Вячеслав Мачулин <machulin.vya4eslav@ya.ru>
2023-03-18 12:44:30 +0300

    tests and build_and_push_to_docker_hub

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -7,13 +7,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY', 'abrakadabra')
 
-# 11:47
+# 12:44
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', False)
 
+ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS', 'localhost')]
 
-# ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS', 'localhost')]
-ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
