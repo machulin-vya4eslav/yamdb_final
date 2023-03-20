9891d7eb62fa7bfaa0c6fc0454f83d05291b3870
Вячеслав Мачулин <machulin.vya4eslav@ya.ru>
2023-03-20 13:57:41 +0300

    final

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY', 'abrakadabra')
 
-# 12:44
+# 13:49
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', False)
 
